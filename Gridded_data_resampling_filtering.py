156e4381e712d60983c154abbfaec8a6a1082f4c
Samuel Russell <69426323+Samuel-Russell@users.noreply.github.com>
2023-02-07 07:57:03 +0100

    Update Gridded_data_resampling_filtering.py
    Second commit

--- Gridded_data_resampling_filtering.py
+++ Gridded_data_resampling_filtering.py
@@ -17,9 +17,42 @@ os.lisdir('EXAMPLE_DATASET')
 
 # Import Data
 fn = 'FILE NAME' with rio.open(fn, 'r') as src:
-rgb = src.read() rgb_meta = src.meta src.close
+    rgb = src.read() 
+    rgb_meta = src.meta 
+    src.close
+
 #Subset
 Subset_rgb_data = rgb[:, 1400:1600, 150:350] print(rgb_meta)
 plt.figure(figsize=(8, 12)) plt.imshow(np.moveaxis(Subset_rgb_data, 0, -1));
 
 
+# data is in data/Golm-UAV
+fn = 'FILE_LOCATION'
+
+with rio.open(fn, 'r') as src: 
+    rgb = src.read() 
+    rgb_meta = src.meta 
+    src.close 
+
+#Subset
+
+Drone_Image_2 = rgb[:, 2350:2700, 1320:1700] 
+print(rgb_meta) 
+plt.figure(figsize=(8, 12))
+plt.imshow(np.moveaxis(Drone_Image_2, 0, -1));
+
+# TASK 2
+# data is in data/Golm-UAV
+fn = 'FILE_LOCATION'
+with rio.open(fn, 'r') as src: 
+    rgb = src.read() 
+    rgb_meta = src.meta 
+    src.close 
+    
+#Subset
+Drone_airlidar = rgb[:, 1400:1600, 150:350] 
+plt.figure(figsize=(8, 12))
+plt.imshow(np.moveaxis(Drone_airlidar, 0, -1));
+
+#Check data shape
+Drone_airlidar.shape
